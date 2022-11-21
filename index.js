92d3fea6f3d18426d3254cb48ac05adbbe83b913
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2022-11-21 20:04:14 +0600

    Connect mongodb with server

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // Middleware 
 app.use(cors());
 app.use(express.json())
 
+
+
+const uri = `mongodb+srv://${proccess.env.DB_USER}:${proccess.env.DB_PASS}@cluster0.ztnz7id.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 
@@ -17,5 +33,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('HPFServer running properly')
+    console.log('HPFServer running properly at ----', port)
 })
